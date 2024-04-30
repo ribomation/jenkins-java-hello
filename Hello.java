1c0b95b750fd8a1e7a5118816c495cae68e7de0c
Ribomation <jens.riboe@ribomation.se>
2024-04-30 10:16:10 +0200

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from a small Java program");
+        System.out.println("Hello from a tiny Java app");
     }
 }
